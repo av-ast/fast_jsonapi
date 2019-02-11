3af982cea692dcf27fb4fdd6fd99c0b1c9c82f13
Alexey Astafyev <av.astafyev@gmail.com>
2019-02-11 15:30:25 +0400

    fix

--- lib/fast_jsonapi/object_serializer.rb
+++ lib/fast_jsonapi/object_serializer.rb
@@ -37,15 +37,16 @@ module FastJsonapi
     alias_method :to_hash, :serializable_hash
 
     def hash_for_one_record
-      serializable_hash = { data: nil }
-      serializable_hash[:meta] = @meta if @meta.present?
-      serializable_hash[:links] = @links if @links.present?
+      # serializable_hash = { data: nil }
+      # serializable_hash[:meta] = @meta if @meta.present?
+      # serializable_hash[:links] = @links if @links.present?
 
-      return serializable_hash unless @resource
+      # return serializable_hash unless @resource
 
-      serializable_hash[:data] = self.class.record_hash(@resource, @fieldsets[self.class.record_type.to_sym], @params)
-      serializable_hash[:included] = self.class.get_included_records(@resource, @includes, @known_included_objects, @fieldsets, @params) if @includes.present?
-      serializable_hash
+      # serializable_hash[:data] = self.class.record_hash(@resource, @fieldsets[self.class.record_type.to_sym], @params)
+      # serializable_hash[:included] = self.class.get_included_records(@resource, @includes, @known_included_objects, @fieldsets, @params) if @includes.present?
+      # serializable_hash
+      self.class.record_hash(@resource, @fieldsets[self.class.record_type.to_sym], @params)
     end
 
     def hash_for_collection
@@ -59,11 +60,12 @@ module FastJsonapi
         included.concat self.class.get_included_records(record, @includes, @known_included_objects, @fieldsets, @params) if @includes.present?
       end
 
-      serializable_hash[:data] = data
-      serializable_hash[:included] = included if @includes.present?
-      serializable_hash[:meta] = @meta if @meta.present?
-      serializable_hash[:links] = @links if @links.present?
-      serializable_hash
+      # serializable_hash[:data] = data
+      # serializable_hash[:included] = included if @includes.present?
+      # serializable_hash[:meta] = @meta if @meta.present?
+      # serializable_hash[:links] = @links if @links.present?
+      # serializable_hash
+      data
     end
 
     def serialized_json
